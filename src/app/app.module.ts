9b835ddf363def96c9ca328034ff587be29ec2b7
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-16 20:35:53 +0200

    Create AuthService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from './shared/services/auth/auth.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
