2dbd7457ed9a162c6e7ff8abe654a075e131b070
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-16 20:29:29 +0200

    Clear AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'spotify-playlist-manager-plus';
-}
+export class AppComponent {}
