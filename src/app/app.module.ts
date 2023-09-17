c9722c0016450711dc4f4f9176cd60b070edd5f0
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-17 23:04:13 +0200

    Import HttpClientModule, add AuthService provider, add environment providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './shared/services/auth/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
-  providers: [AuthService],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
+  providers: [
+    { provide: 'GRANT_TYPE', useValue: environment.grantType },
+    { provide: 'CLIENT_ID', useValue: environment.clientId },
+    { provide: 'CLIENT_SECRET', useValue: environment.clientSecret },
+    AuthService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
