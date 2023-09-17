4bcc7f53a9adf9ce09ac24a9067aeff81cc640a6
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-17 23:05:19 +0200

    Implement #fetchAccessToken

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenResponse } from '../../model/token-response.type';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(
+    @Inject('GRANT_TYPE') public grantType: string,
+    @Inject('CLIENT_ID') public clientId: string,
+    @Inject('CLIENT_SECRET') public clientSecret: string,
+    private httpClient: HttpClient,
+  ) {}
+
+  fetchAccessToken(): Observable<TokenResponse> {
+    return this.httpClient.post<TokenResponse>(
+      'https://accounts.spotify.com/api/token',
+      `grant_type=${this.grantType}&client_id=${this.clientId}&client_secret=${this.clientSecret}`,
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      },
+    );
+  }
 }
