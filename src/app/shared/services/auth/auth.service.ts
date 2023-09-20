ffb63982a6b08974c0710d976d91d5488e36cadc
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-20 20:15:44 +0200

    Add #hashTextUsingSHA256

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TokenResponse } from '../../models/token-response.type';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +16,9 @@ export class AuthService {
   ) {}
 
   fetchAccessToken(): Observable<TokenResponse> {
+    const codeVerifier = this.generateCodeVerifier();
+    const codeVerifierHash = this.hashTextUsingSHA256(codeVerifier);
+
     throw Error('Fetching authorization code with PKCE flow is not implemented yet.');
   }
 
@@ -22,5 +26,9 @@ export class AuthService {
     const array = new Uint32Array(56 / 2);
     window.crypto.getRandomValues(array);
     return Array.from(array, (n) => ('0' + n.toString(16)).substring(-2)).join('');
+  }
+
+  private hashTextUsingSHA256(plainText: string): string {
+    return CryptoJS.SHA256(plainText).toString();
   }
 }
