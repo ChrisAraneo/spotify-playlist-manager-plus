a557f62de4b2b912880c7940205b92115ae18e87
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-27 23:15:33 +0200

    Add playlists route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaylistListComponent } from './playlist-list/playlist-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'playlists',
+    component: PlaylistListComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'playlists',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
